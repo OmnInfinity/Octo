bebcaa04142a7e82d4c973cc470f147e4cb90a61
Kevin Pho <kevinkhoapho@gmail.com>
2016-11-27 16:50:28 -0800

    [Rev] repository

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿///// ///// ///// Octo.cs ///// ///// /////
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2017 Kevin Pho (OmnInfinity) <kevinkhoapho@gmail.com>,
+ *                         CAMS VEX 687 "Nerd Herd",
+ *                         CAMS VEX 687-C
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Collections.Generic;
+
+/*
+ * Compiler framework
+ *
+ * @author Kevin Pho
+ */
+namespace Team687.Octo
+{
+  class Program
+  {
+    public static int Success = 0;
+    public static int Failure = 1;
+
+    static int Main(string[] args)
+    {
+      Console.WriteLine("Octo by OmnInfinity");
+
+      Token digit = new Token("0123456789");
+      Token period = new Token(".");
+      Token run = new Token().Build("run");
+
+      Machine number = new Machine() {
+        digit,
+        period,
+        run,
+      };
+
+      digit.start();
+      digit.enter = delegate bool()
+      { };
+
+
+
+      State<string> digit = new State<string>("0213456789");
+      State<string> period = new State<string>(".");
+      State<string> test = new State<string>.Build("test");
+
+      Table states = new Table() {
+        { "q0", new State("q0").Start().End() },
+        { "q1", new State("q1")               },
+        { "q2", new State("q2")        .End() },
+      };
+
+      Machine<string> asimov = new Machine<string>().AddStates({
+        digit,
+        period,
+        test
+      });
+
+      // The set of causes and effects for states
+      Cause a = delegate (object input) { return "a".Contains((string) input); };
+      Effect q0 = delegate (bool input) { return (string) "No input"; };
+      Effect q2 = delegate (bool input) { return (string) "Even"; };
+
+      // The set of transitions between states
+      states["q0"].Connect(a, states["q1"], q0);
+      states["q1"].Connect(a, states["q2"], q2);
+      states["q2"].Connect(a, states["q1"], null);
+
+      // The constructed state automaton
+      Machine token = new Machine("Token", states);
+
+      // Test strings through state automaton
+      // token.Accepts("");
+      // token.Accepts("a");
+      // token.Accepts("aa");
+
+      Console.ReadKey();
+      return Program.Success;
+    }
+
+    static int Update(string[] args) {
+      return Program.Success;
+    }
+  }
+}
---
+++ State.cs
@@ -0,0 +1,116 @@
+﻿///// ///// ///// State.cs ///// ///// /////
+
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016-2017 Kevin Pho (OmnInfinity) <kevinkhoapho@gmail.com>,
+ *                         CAMS VEX 687 "Nerd Herd",
+ *                         CAMS VEX 687-C
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace Team687.Octo
+{
+  public delegate bool Cause(object input);
+
+  public delegate object Effect(bool input);
+
+  public interface Connectable<T>
+  {
+    T Connect(Cause choice, T output, Effect result);
+  }
+
+  public class Table : Dictionary<string, State> { }
+
+  public class Machine
+  {
+    public string Name { get; set; }
+    public Table States { get; set; }
+    public List<string> Queue { get; set; }
+
+    public Machine(string name, Table states)
+    {
+      this.Name = name;
+      this.States = states;
+      this.Queue = new List<string>();
+
+      foreach (State state in new List<State>(states.Values))
+      {
+        if (state.Acidic)
+        {
+          this.Queue.Add(state.Name);
+        }
+      }
+    }
+
+    /* TODO */
+    // public bool Traverse(T ype) { return true; }
+
+    /* TODO */
+    // public bool Accepts(T ype) { return true; }
+  }
+
+  public class State : Connectable<State>
+  {
+    public string Name { get; set; }
+    public int Priority { get; set; }
+    public List<Tuple<Cause, State, Effect>> Connections { get; set; }
+    public bool Acidic { get; set; }
+    public bool Basic { get; set; }
+    public delegate void Entered();
+    public delegate void Exited();
+
+    public State(string name, int priority = 0)
+    {
+      this.Name = name;
+      this.Priority = priority;
+      this.Connections = new List<Tuple<Cause, State, Effect>>();
+      this.Acidic = false;
+      this.Basic = false;
+    }
+
+    public State Start()
+    {
+      this.Acidic = true;
+      return this;
+    }
+
+    public State End()
+    {
+      this.Basic = true;
+      return this;
+    }
+
+    public State Connect(Cause choice, State output, Effect result)
+    {
+      Tuple<Cause, State, Effect> connection = new Tuple<Cause, State, Effect>(choice, output, result);
+
+      if (!this.Connections.Contains(connection))
+      {
+        this.Connections.Add(connection);
+      }
+
+      return this;
+    }
+  }
+}
